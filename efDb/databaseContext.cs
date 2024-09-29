0bf124beb9b3d01b5e5e82ec4969cfb02b61293f
Christian <christian.lsj@outlook.dk>
2024-09-29 18:47:23 +0200

    Added ProductRating model to entity framework

--- efDb/databaseContext.cs
+++ efDb/databaseContext.cs
@@ -7,6 +7,7 @@ public class DatabaseContext :DbContext
 
     // Db Context
     public DbSet<Product> Products {get;set;}
+    public DbSet<ProductRating> ProductRatings { get; set; }
 
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
