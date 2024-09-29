866fd21bfe62200dc6199e1a6a89dedb5c5b6659
Christian <christian.lsj@outlook.dk>
2024-09-29 13:26:21 +0200

    Added product table

--- databaseInitial.sql
+++ databaseInitial.sql
@@ -1,2 +1,10 @@
 -- Christian l.s.j 2024
 -- containing all database Initial commands --
+
+
+-- Product table
+CREATE TABLE products(
+  Id int PRIMARY key AUTO_INCREMENT,
+  Name VARCHAR(86) NOT NULL,
+  Price DECIMAL(10,2) NOT NULL
+);
