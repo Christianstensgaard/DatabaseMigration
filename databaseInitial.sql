05c1f690bcc2cc0578a62683e45efd595947ddbd
Christian <christian.lsj@outlook.dk>
2024-09-29 13:53:34 +0200

    Added Categories & Modifyed Products

--- databaseInitial.sql
+++ databaseInitial.sql
@@ -1,2 +1,16 @@
 -- Christian l.s.j 2024
 -- containing all database Initial commands --
+
+-- Categories table
+CREATE TABLE Categories (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    CategoryName VARCHAR(86) NOT NULL
+);
+
+--Modify Products table
+ALTER TABLE Products
+  ADD COLUMN category_id INT,
+  ADD CONSTRAINT fk_category
+    FOREIGN KEY (category_id)
+    REFERENCES Categories(Id);
+
