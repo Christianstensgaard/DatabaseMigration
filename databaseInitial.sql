3487a14125af9da9794465ec0270d99a89da126e
Christian <christian.lsj@outlook.dk>
2024-09-29 14:52:45 +0200

    Added products rating

--- databaseInitial.sql
+++ databaseInitial.sql
@@ -1,2 +1,14 @@
 -- Christian l.s.j 2024
 -- containing all database Initial commands --
+
+
+-- Product Ratings
+CREATE TABLE ProductRatings (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    ProductId INT NOT NULL,
+    Rating INT CHECK (Rating >= 1 AND Rating <= 5),
+    CONSTRAINT fk_product
+        FOREIGN KEY (ProductId)
+        REFERENCES Products(Id)
+);
+
